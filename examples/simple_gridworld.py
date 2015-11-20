33ce17ee3f997149d958d7ea1131a4502ac39d97
Billy Okal <okal.billy@gmail.com>
2015-11-20 19:53:21 +0100

    additing more utilities to data structures

--- examples/simple_gridworld.py
+++ examples/simple_gridworld.py
@@ -47,6 +47,16 @@ def main():
     plt.imshow(vmap, interpolation='nearest', cmap='viridis')
     plt.colorbar()
 
+    ##
+    plt.figure()
+    X, Y = np.meshgrid(np.arange(0, 2 * np.pi, .2), np.arange(0, 2 * np.pi, .2))
+    U = np.cos(X)
+    V = np.sin(Y)
+
+    Q = plt.quiver(U, V)
+    qk = plt.quiverkey(Q, 0.5, 0.92, 2, r'$2 \frac{m}{s}$', labelpos='W',
+                       fontproperties={'weight': 'bold'})
+
     plt.show()
 
 
