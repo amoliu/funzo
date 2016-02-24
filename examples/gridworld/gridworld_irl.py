1ecc3985c58af457afcc6200f37130bca21041a5
Billy Okal <okal.billy@gmail.com>
2016-02-24 23:37:12 +0100

    Make irl example conform to new api

--- examples/gridworld/gridworld_irl.py
+++ examples/gridworld/gridworld_irl.py
@@ -1,12 +1,16 @@
 
 
+import matplotlib
+matplotlib.use('Qt4Agg')
+
 from matplotlib import pyplot as plt
 plt.style.use('fivethirtyeight')
 
 import numpy as np
 
-from funzo.domains.gridworld import GridWorld, GRewardLFA, GReward
-from funzo.planners.dp import policy_iteration
+from funzo.domains.gridworld import GridWorld, GridWorldMDP
+from funzo.domains.gridworld import GRewardLFA, GReward, GTransition
+from funzo.planners.dp import PolicyIteration
 
 from funzo.irl.birl.map_birl import MAPBIRL
 from funzo.irl.birl.base import GaussianRewardPrior
@@ -16,19 +20,21 @@ SEED = None
 
 
 def main():
-    gmap = np.loadtxt('maps/map_c.txt')
+    gmap = np.loadtxt('maps/map_b.txt')
     # w = np.array([0.001, -0.1, 1.0])
     w = np.array([-0.001, -0.1, 1.0])
 
-    g = GridWorld(gmap, reward_function=None, discount=0.7)
+    world = GridWorld(gmap=gmap)
+    rfunc = GReward(domain=world)
     # rfunc = GRewardLFA(g, weights=w)
+    T = GTransition(domain=world)
+    g = GridWorldMDP(domain=world, reward=rfunc, transition=T, discount=0.7)
 
-    rfunc = GReward(g)
-    g._reward = rfunc
     w = rfunc._R
 
     # ------------------------
-    plan = policy_iteration(g, verbose=1)
+    planner = PolicyIteration(verbose=2)
+    plan = planner(g)
     policy = plan['pi']
     print(policy)
 
@@ -37,7 +43,7 @@ def main():
     # ax = g.visualize(ax, policy=policy)
     # plt.show()
 
-    demos = g.generate_trajectories(policy, num=50, random_state=SEED)
+    demos = world.generate_trajectories(policy, num=50, random_state=SEED)
     # demos = g.generate_trajectories(policy, starts=[1, 4, 3], random_state=SEED)
     # np.save('demos.npy', demos)
     # demos = np.load('demos.npy')
@@ -45,13 +51,12 @@ def main():
 
     # IRL
     r_prior = GaussianRewardPrior(sigma=0.15)
-    irl_solver = MAPBIRL(mdp=g, prior=r_prior, demos=demos,
-                         planner=policy_iteration,
+    irl_solver = MAPBIRL(mdp=g, prior=r_prior, demos=demos, planner=planner,
                          beta=0.6)
     r, data = irl_solver.run(V_E=plan['V'], random_state=SEED)
 
     g.reward.weights = r
-    r_plan = policy_iteration(g)
+    r_plan = planner(g)
     print(r_plan['pi'])
     print(r)
     V = r_plan['V']
@@ -64,7 +69,7 @@ def main():
     # ------------------------
     fig = plt.figure(figsize=(8, 8))
     ax = fig.gca()
-    ax = g.visualize(ax, policy=r_plan['pi'])
+    ax = world.visualize(ax, policy=r_plan['pi'])
 
     plt.figure(figsize=(8, 8))
     plt.imshow(V.reshape(gmap.shape),
