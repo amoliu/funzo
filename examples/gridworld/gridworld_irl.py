MAINT Fix lfa weight normalization

examples/gridworld/gridworld_irl.py
@@ -27,15 +27,17 @@ SEED = None
 
 def main():
     gmap = np.loadtxt('maps/map_b.txt')
-    w_expert = np.array([-0.001, -0.5, 1.0])
-    # w_expert = np.array([0.0001, 0.0, 1.0])
+    # w_expert = np.array([-0.001, -0.5, 1.0])
+    w_expert = np.array([-0.01, -10.0, 1.0])
+    w_expert /= (w_expert.max() - w_expert.min())
 
     world = GridWorld(gmap=gmap)
     # rfunc = GReward(domain=world, rmax=1.0/len(world.states))
     rfunc = GRewardLFA(domain=world, weights=w_expert,
                        rmax=1.0/len(world.states))
+
     T = GTransition(domain=world)
-    g = GridWorldMDP(domain=world, reward=rfunc, transition=T, discount=0.9)
+    g = GridWorldMDP(domain=world, reward=rfunc, transition=T, discount=0.8)
 
     # w_expert = rfunc._R
 
@@ -50,7 +52,7 @@ def main():
     # ax = world.visualize(ax, policy=policy)
     # plt.show()
 
-    demos = world.generate_trajectories(policy, num=10, random_state=SEED)
+    demos = world.generate_trajectories(policy, num=20, random_state=SEED)
     # np.save('demos.npy', demos)
     # demos = np.load('demos.npy')
     # print(demos)
@@ -59,25 +61,25 @@ def main():
     r_prior = GaussianRewardPrior(sigma=0.15)
     # irl_solver = MAPBIRL(mdp=g, prior=r_prior, demos=demos, planner=planner,
     #                      beta=0.6)
-    irl_solver = PolicyWalkBIRL(mdp=g, prior=r_prior, demos=demos, delta=0.2,
-                                planner=planner, beta=0.6, max_iter=1500)
+    irl_solver = PolicyWalkBIRL(mdp=g, prior=r_prior, demos=demos, delta=0.32,
+                                planner=planner, beta=0.6, max_iter=5000)
     # r, data = irl_solver.run(random_state=SEED)
     trace, mr = irl_solver.run(random_state=SEED)
     trace.save('pw_trace')
     r = mr[-1]
 
-    g.reward.weights = r
+    g.reward.update_parameters(reward=r)
     r_plan = planner(g)
     print(r_plan['pi'])
-    print(r)
+    print('Found reward: {}'.format(r))
     V = r_plan['V']
 
     # compute the loss
     # loss_func = PolicyLoss(mdp=g, planner=planner, order=1)
     loss_func = RewardLoss(order=2)
     # loss = [loss_func(w_expert, w_pi) for w_pi in data['rewards']]
-    # loss = [loss_func(w_expert, w_pi) for w_pi in trace['r']]
-    loss = [loss_func(w_expert, w_pi) for w_pi in mr]
+    loss = [loss_func(w_expert, w_pi) for w_pi in trace['r']]
+    # loss = [loss_func(w_expert, w_pi) for w_pi in mr]
 
     # ------------------------
     fig = plt.figure(figsize=(8, 8))
@@ -94,7 +96,7 @@ def main():
 
     plt.figure(figsize=(8, 6))
     # plt.plot(data['iter'], loss)
-    plt.plot(loss)
+    plt.plot(trace['step'], loss)
     plt.ylabel('Loss function $\mathcal{L}_{\pi}$')
     plt.xlabel('Iteration')
     plt.tight_layout()

funzo/models/mdp.py
@@ -307,8 +307,7 @@ class LinearRewardFunction(six.with_metaclass(ABCMeta, RewardFunction)):
         assert self._weights.ndim == 1, 'Weights must be 1D arrays'
 
         # ensure reward is bounded by normalizing the weights
-        # self._weights /= np.sum(self._weights)
-        # self._weights *= self.rmax
+        self._weights /= (self._weights.max() - self._weights.min())
 
     def update_parameters(self, **kwargs):
         """ Update the weights parameters of the reward function model """
@@ -316,8 +315,7 @@ class LinearRewardFunction(six.with_metaclass(ABCMeta, RewardFunction)):
             w = np.asarray(kwargs['reward'])
             assert w.shape == self._weights.shape,\
                 'New weight array size must match reward function dimension'
-            w /= np.sum(w)
-            w *= self.rmax
+            w /= (w.max() - w.min())
             self._weights = w
 
     @property