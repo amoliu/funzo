b3b0ddce950d89f47ef1837c8e0a0f550217206c
Billy Okal <okal.billy@gmail.com>
2016-04-19 00:54:53 +0200

    TST improving trace data structure

--- funzo/utils/data_structures.py
+++ funzo/utils/data_structures.py
@@ -1,9 +1,5 @@
 """
-Data structure helpers
-========================
-
-1. Trace -- MCMC sampling logs
-
+Utility data structures
 """
 
 from __future__ import division, absolute_import
@@ -24,39 +20,47 @@ class Trace(object):
 
     Parameters
     -----------
-    save_interval : int
+    save_interval : int, optional (default: 100)
         No. of iterations before a periodic save of current data
-    variables : list of string
-        Names of all the variables stored
+    variables : list of string or str, optional (default: None)
+        Names of all the variables stored, a single variable name
 
      Attributes
     ------------
     _save_interval : int
-        No. of iterations before a periodic save of current data
+        No. of iterations before a periodic save of current data. Zero means
+        no periodic saves
     _variables : list of string
         Names of all the variables stored
 
     """
 
-    def __init__(self, variables, save_interval=100):
+    def __init__(self, variables=None, save_interval=100):
         if save_interval < 0:
-            raise ValueError('Saving interval must be > 0')
+            raise ValueError('Saving interval must be >= 0')
         if save_interval > 1000:
             warnings.warn('Very large saving interval could result in\
                           loss of data in a crash')
 
-        if not isinstance(variables, Iterable):
-            raise ValueError('*variables* must be an iterable container')
-
-        if not all([isinstance(v, str) for v in variables]):
-            raise TypeError('All *variables* must be strings type')
-
         self._save_interval = save_interval
-        self._vars = variables
-
         self._data = dict()
-        for v in self._vars:
-            self._data[v] = list()
+        self._vars = None
+
+        if variables is not None:
+            if not isinstance(variables, Iterable):
+                raise ValueError('*variables* must be an iterable container')
+
+            if not all([isinstance(v, str) for v in variables]):
+                raise TypeError('All *variables* must be strings type')
+
+            if isinstance(variables, str):
+                warnings.warn('Single variable assigned. Assuming a single\
+                               element trace store')
+                variables = list(variables)
+
+            self._vars = variables
+            for v in self._vars:
+                self._data[v] = list()
 
         self._old_save = None
         self._iter = 0
@@ -76,8 +80,10 @@ class Trace(object):
 
         self._iter += 1
 
-        if self._iter > 0 and self._iter % self._save_interval == 0:
-            self.save('trace')
+        periodic_save = self._save_interval > 0
+        if periodic_save:
+            if self._iter > 0 and self._iter % self._save_interval == 0:
+                self.save('trace')
 
     def save(self, filename='trace', final=False):
         """ Save trace as an HDF5 file with groups """
@@ -85,7 +91,7 @@ class Trace(object):
             saved_name = '{}.hdf5'.format(filename)
         else:
             saved_name = '{}_{}.hdf5'.format(filename, time_string())
-        f = h5py.File(saved_name, 'w')
+        f = h5py.File(saved_name, mode='w')
         for key in self._data:
             f[key] = self._data[key]
         f.close()
@@ -99,12 +105,12 @@ class Trace(object):
     def load(self, filename):
         """ Load a trace from file """
         self._vars = list()
-        self._data = dict
+        self._data = dict()
 
-        f = h5py.File(filename, 'r')
+        f = h5py.File(filename, mode='r')
         for key in f:
             self._vars.append(key)
-            self._data[key] = f[key]
+            self._data[key] = f[key][:]
         f.close()
 
     def add_vars(self, new_vars):
@@ -122,9 +128,12 @@ class Trace(object):
         raise NotImplementedError('Not yet implemented')
 
     def __getitem__(self, item):
-        if item not in self._data:
+        if item not in self:
             raise ValueError('Invalid key: {} not available'.format(item))
         return self._data[item]
+
+    def __contains__(self, key):
+        return key in self._vars if self._vars is not None else False
 
 
 def time_string():
--- funzo/utils/tests/test_data_structures.py
+++ funzo/utils/tests/test_data_structures.py
@@ -4,16 +4,27 @@ import os
 
 import numpy as np
 
-from nose.tools import assert_raises
+from nose.tools import assert_raises, assert_is_instance
 from numpy.testing import assert_equal
 
 from funzo.utils.data_structures import Trace
 
 
 def test_trace_init():
-    """ Test for proper instantiated of Trace object """
+    """ Test for proper instantiated of a Trace """
     t = Trace(variables=['step'])
     assert len(t['step']) == 0
+
+    t2 = Trace(variables=['step'], save_interval=0)  # check no periodic
+    assert len(t['step']) == 0
+
+    assert_is_instance(t, Trace)
+    assert_is_instance(t2, Trace)
+
+    assert_raises(ValueError, t.__init__,
+        variables=['step'], save_interval=-1)
+    #assert_raises(ValueError, t.__init__, variables='step')
+    assert_raises(TypeError, t.__init__, variables=['step', 4])
 
 
 def test_Trace_record():
@@ -34,7 +45,7 @@ def test_Trace_record():
 
 
 def test_trace_save():
-    """ Test save function of Trace """
+    """ Test saving Trace """
     t = Trace(variables=['step', 'r', 'q'])
 
     np.random.seed(42)
@@ -54,7 +65,36 @@ def test_trace_save():
     os.remove(fname)
 
 
+def test_trace_load():
+    """ Test loading Trace """
+    t = Trace(variables=['step', 'r', 'q'])
+
+    np.random.seed(42)
+    qq = np.random.rand(5)
+    t.record(step=1, r=[1.0, 2.0], q=qq)
+    fname = t.save('trace', final=True)
+
+    # load in a new trace instance
+    t2 = Trace()
+    t2.load(fname)
+    assert 'r' in t2.vars
+    assert 'q' in t2.vars
+    assert len(t2['r']) == 1
+    assert_equal(t2['r'][0], [1.0, 2.0])
+    assert_equal(t2['q'][0], qq)
+
+    os.remove(fname)
+
+
 def test_trace_getitem():
-    """ Test __getitem__ function of Trace """
+    """ Test __getitem__ (indexing) function of Trace """
     t = Trace(variables=['r'])
     assert_raises(ValueError, t.__getitem__, 'V')
+
+
+def test_trace_membership():
+    """ Test __contains__ (membership) function of Trace """
+    t = Trace(variables=['r'])
+    assert 'r' in t
+    assert 'x' not in t
+    assert 10 not in t
